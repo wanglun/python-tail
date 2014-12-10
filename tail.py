42aea9851581542d6a5056f37e67ce1fd0746a99
wanglun <iseuer@gmail.com>
2014-12-10 13:17:15 +0800

    callback with extra offset and fp args

--- tail.py
+++ tail.py
@@ -39,24 +39,26 @@ class Tail(object):
         self.tailed_file = tailed_file
         self.callback = sys.stdout.write
 
-    def follow(self, s=1):
+    def follow(self, s=1, offset=0, whence=2):
         ''' Do a tail follow. If a callback function is registered it is called with every new line. 
         Else printed to standard out.
     
         Arguments:
-            s - Number of seconds to wait between each iteration; Defaults to 1. '''
+            s - Number of seconds to wait when readline fail; Defaults to 1. '''
 
         with open(self.tailed_file) as file_:
             # Go to the end of file
-            file_.seek(0,2)
+            file_.seek(offset, whence)
             while True:
                 curr_position = file_.tell()
                 line = file_.readline()
+
+                if self.callback(line=line, offset=curr_position, fp=file_) is False:
+                    break
+
                 if not line:
                     file_.seek(curr_position)
-                else:
-                    self.callback(line)
-                time.sleep(s)
+                    time.sleep(s)
 
     def register_callback(self, func):
         ''' Overrides default callback function to provided function. '''
